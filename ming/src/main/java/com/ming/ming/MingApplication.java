03732de6c33d3d25326e51a8b3f39bd971b958b1
ming <18120580001@163.com>
2018-08-14 15:33:14 +0800

    初始化 ming show项目

---
+++ ming/src/main/java/com/ming/ming/MingApplication.java
@@ -0,0 +1,12 @@
+package com.ming.ming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingApplication.class, args);
+	}
+}

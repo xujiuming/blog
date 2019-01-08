ef89a2f99c133e5a6e494cf70a042447f7937d91
ming <18120580001@163.com>
2019-01-08 10:04:43 +0800

    删除ming模块  、  更新config

--- ming/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dd = $(date +%Y%m%d)
-sudo mvn -DskipTests install
-dockerVersion = registry.cn-hangzhou.aliyuncs.com/jiuming/ming:${dd}
-sudo docker build -t ${dockerVersion}
-sudo docker push ${dockerVersion}
-

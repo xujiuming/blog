8075da711d5d01574f9d619d1c55f60ffbd0cf61
ming <18120580001@163.com>
2019-05-30 18:02:56 +0800

    处理tag 云 兼容oss

--- themes/hexo-theme-next-7.1.1/scripts/ming-Plugin-config.js
+++ themes/hexo-theme-next-7.1.1/scripts/ming-Plugin-config.js
@@ -7,5 +7,8 @@ hexo.extend.helper.register('paginator', require('./ming-paginator'));
 hexo.extend.helper.register('list_categories', require('./ming_categories'));
 hexo.extend.helper.register('list_archives', require('./ming_archives'));
 hexo.extend.helper.register('list_tags', require('./ming_tags'));
+hexo.extend.helper.register('list_posts', require('./ming_posts'));
+hexo.extend.helper.register('tag_cloud', require('./ming_tagcloud'));
+hexo.extend.helper.register('tagcloud', require('./ming_tagcloud'));
 
 

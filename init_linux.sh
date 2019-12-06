42f527d22f38bf7ac403baa2d96fb5b18334a15c
ming <18120580001@163.com>
2019-12-06 16:43:33 +0800

    doc (新增文章)： 添加文章
    add 基于githubActions、oss自动化部署静态站点.md
    delete 删除无用文件

--- init_linux.sh
+++ init_linux.sh
@@ -2,18 +2,9 @@
 #安装npm
 sudo pacman -Syu npm
 #安装 cnpm
-sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
-# 使用npm 安装  n
-sudo cnpm install -g n
-# 使用n  更新 npm  node
-sudo n latest
-#安装 hexo
-sudo cnpm install hexo -g
-# 安装压缩优化
-sudo cnpm install gulp -g
+sudo npm install --registry=https://registry.npm.taobao.org
+sudo npm install -g hexo-cli --registry=https://registry.npm.taobao.org
 
-#安装依赖
-sudo cnpm install
 
 
 ########################################################################################################
